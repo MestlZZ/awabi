f2e709940e9e5991ca77b486d7a4f809ec87ea0d
Богдан <bogdan.gork@gmail.com>
2015-11-12 02:33:52 +0200

    Реализована форма для быстрого набора инфы и ее отправки (без CSS)

--- WCS.Controllers/SendFormController.cs
+++ WCS.Controllers/SendFormController.cs
@@ -9,10 +9,16 @@ namespace WCS.Controllers
 {
     public class SendFormController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             ViewBag.Title = "Відправка форми";
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(SendForm send)
+        {
+            return RedirectToRoute( new { controller = "Home", action = "Index" } );
         }
     }
 }
