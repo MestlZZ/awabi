2e2cd8147f8a58193028d2fbff07e9e208566705
Богдан <bogdan.gork@gmail.com>
2015-11-12 05:08:37 +0200

    Исправлена очистка базы

--- WCS.Database/SendFormDbInitializer.cs
+++ WCS.Database/SendFormDbInitializer.cs
@@ -7,11 +7,11 @@ using WCS.Models;
 
 namespace WCS.Databases
 {
-    public class SendFormDbInitializer : DropCreateDatabaseAlways<SendFormContext>
+    public class SendFormDbInitializer : DropCreateDatabaseIfModelChanges<SendFormContext>
     {
         protected override void Seed( SendFormContext db )
         {
-            db.SendForms.Add( new SendForm { Award = 120, SityID="12", TaitionFee=120, RentsHostel=120, RentsWithoutF=120, InstitutionID="non", ExpensesWithoutF=120, ExpensesWithF=120, ExpensesHostel=120 } );
+            db.SendForms.Add( new SendForm { Id = 1, Award = 120, SityID="12", TaitionFee=120, RentsHostel=120, RentsWithoutF=120, InstitutionID="non", ExpensesWithoutF=120, ExpensesWithF=120, ExpensesHostel=120 } );
             base.Seed( db );
         }
     }
