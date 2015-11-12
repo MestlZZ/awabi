8d836be19312621d50219a0404ffb9e81c966329
Богдан <bogdan.gork@gmail.com>
2015-11-12 02:40:33 +0200

    Реализована модель подключения к базе

---
+++ WCS.Database/SendFormContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Data.Entity;
+using WCS.Models;
+
+namespace WCS.Database
+{
+    public class SendFormContext : DbContext
+    {
+        public DbSet<SendForm> SendForms { get; set; }
+    }
+}
