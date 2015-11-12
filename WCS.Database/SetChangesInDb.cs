98ad64d24cb9332bbf67c133505d98aed3450f11
Богдан <bogdan.gork@gmail.com>
2015-11-12 03:32:01 +0200

    Добавлены методы для работы с базой

---
+++ WCS.Database/SetChangesInDb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using WCS.Models;
+
+namespace WCS.Database
+{
+    public class SetChangesInDb
+    {
+        SendFormContext db = new SendFormContext();
+        public void SaveInDb(SendForm form)
+        {
+            db.SendForms.Add(form);
+            db.SaveChanges();
+        }
+        public void UpdateInDb(SendForm form)
+        {
+            db.Entry(form).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+        public void DeleteInDb(SendForm form)
+        {
+            db.SendForms.Remove( form );
+            db.SaveChanges();
+        }
+    }
+}
