ba0c7d81e531147f958941ce62bae27a7a8038f4
Yaroslav Yachmenov <yach.yaroslav@gmail.com>
2015-11-26 22:10:59 +0200

    404-а сторінка + зміни в структурі по стилям та фонтам

---
+++ WCS.Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WCS.Databases;
+using WCS.Models;
+
+namespace WCS.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult PageNotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
