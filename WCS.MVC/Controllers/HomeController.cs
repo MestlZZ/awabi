5079c18bfd7535b661b9301bfb33c62865d50939
Богдан <bogdan.gork@gmail.com>
2015-12-04 15:37:37 +0200

    главная

--- WCS.MVC/Controllers/HomeController.cs
+++ WCS.MVC/Controllers/HomeController.cs
@@ -17,7 +17,6 @@ namespace WCS.MVC
         }
         public ActionResult AboutUsPage()
         {
-            NotesBusiness.AddUniversityToDb();
             return View();
         }
         public ActionResult SearchPage()
