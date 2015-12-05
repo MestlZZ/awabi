914c51346f9c34facc380e995c3d8d24064871af
Yaroslav Yachmenov <yach.yaroslav@gmail.com>
2015-12-05 14:04:35 +0200

    Профіксив баг із замерзанням форми в edge і explorer 11 + баг із скриптовим undefined

--- WCS/Scripts/AbiturientValidate.js
+++ WCS/Scripts/AbiturientValidate.js
@@ -1,4 +1,11 @@
 ﻿$(function () {
+    var m;
+    if ($ && $.browser && (m = (navigator.userAgent).match(/Edge\/(1[2-9]|[2-9]\d|\d{3,})\./))) {
+        delete($.browser.mozilla);
+        delete($.browser.msie);
+        $.browser.edge = true;
+        $.browser.version = m[1];
+    }
     $(".editor-field").each(function (index) {
         this_input = $(".text-box")[index];
         $(this_input).attr("title", 'Значення від 0 до 5000');
@@ -10,8 +17,9 @@
     $(".editor-field").bind("DOMSubtreeModified", function (index) {
         var error_obj = $(this).children(".field-validation-error");
         if (error_obj.text()) {
-            error_obj.css({ display: 'none' });
             $(this).children(".text-box").attr("data-original-title", error_obj.text());
+            if ($.browser.msie || $.browser.edge) error_obj.text("");
+            else error_obj.hide();
         }
     });
     $("form").validate();
