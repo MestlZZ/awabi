069f006a17407b9b3a14d00f80bf27263b4e59b0
Богдан <bogdan.gork@gmail.com>
2015-12-13 08:16:01 +0200

    Шо маємо то маємо

--- WCS.Models/Feedback.cs
+++ WCS.Models/Feedback.cs
@@ -17,19 +17,19 @@ namespace WCS.Models
         [ScaffoldColumn( false )]
         public DateTime Date { private set; get; }
 
-        [Display( Name = "Ім'я" )]
+        [Display( Name = "Ваше ім'я" )]
         [DataType( DataType.Text )]
         [Required( ErrorMessage = "Будь ласка, вкажіть власне ім'я!" )]
         [StringLength(25, MinimumLength = 2, ErrorMessage = "Ім'я має бути в межаї від 2 до 25 символів")]
         public string Name { get; set; }
 
-        [Display( Name = "Пошта" )]
+        [Display( Name = "Адреса вашої пошти" )]
         [DataType( DataType.EmailAddress )]
         [Required( ErrorMessage = "Будь ласка, заповніть це поле!" )]
         [StringLength( 50, MinimumLength = 4, ErrorMessage = "Максимальна довжина адреси не повинно бути більше ніж 50 символів" )]
         public string Mail { get; set; }
 
-        [Display( Name = "Відгук" )]
+        [Display( Name = "Відгук про сервіс" )]
         [DataType( DataType.MultilineText )]
         [Required( ErrorMessage = "Будь ласка, напишіть відгук!" )]
         [StringLength( 1024, MinimumLength = 5, ErrorMessage = "Довжина повідомлення повинна бути в межах від 5 до 1024 символів" )]
